962697894cb1a8d8b1158daeb169fbe8f34f2fba
lolwhatk <78239811+lolwhatk@users.noreply.github.com>
2021-02-09 18:34:24 +0300

    Add files via upload

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -8,23 +8,23 @@ namespace ConsoleApp1
         {
             DateTime date1 = DateTime.Now;
             double k = date1.Second;
-            double allmin = date1.Hour * 60 + date1.Minute + k / 60;
-
-            int ch = allmin / 78;
-
+            double allmin = date1.Hour * 60 + date1.Minute + k/60;
+           
+            int ch =(int) (allmin / 78);
+           
             double r = allmin - ch * 78;
-            int d = (int)(r / 7.8);
-
-            double r1 = r - d * 7.8;
-            int dol = (int)(r1 / 0.78);
-            double r2 = r1 - dol * 0.78;
-            int vzdox = (int)(r2 / 0.078);
-
+            int d = (int)(r /7.8);
+           
+            double r1 = r - d*7.8;
+            int dol = (int)(r1 /0.78);
+            double r2 = r1 - dol*0.78;
+            int vzdox = (int)(r2 /0.078);
+       
             Time(ch, d, dol, vzdox);
 
         }
-
-        static double Time(int ch, int d, int dol, int vzdox)
+        
+       static int Time(int ch,int d,int dol,int vzdox)
         {
             while (ch < 20)
             {
@@ -34,11 +34,10 @@ namespace ConsoleApp1
                     {
                         while (vzdox < 10)
                         {
-                            i
                             System.Threading.Thread.Sleep(4700);
                             Console.Clear();
                             Console.WriteLine($"{ch}ч {d}д {dol}дл {vzdox}вз");
-                            
+
                             vzdox++;
                         }
                         dol++;
@@ -46,11 +45,11 @@ namespace ConsoleApp1
                     }
                     d++;
                     dol = 0;
-
+                    
                 }
                 ch++;
                 d = 0;
-
+                
             }
 
             return Time(0, 0, 0, 0);
